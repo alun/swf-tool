3c622aafbbc7c5255e9d4572f644b01442bc81b3
Alexey Lunacharsky <alun@katlex.com>
2015-07-04 21:54:11 +0600

    Added routines to check global sound access

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Running
 
-This project requires Scala 2.8.1 because of apparat binary dependencies which doen't seem to be maintained anymore. So you'll need Java 7 to run this without erorrs. Because Scala 2.8 SBT compiler interface isn't built on Java 8.
+This project requires Scala 2.8.1 because of apparat binary dependencies which doesn't seem to be maintained anymore.  
+So you'll need Java 7 to run this without erorrs. Because Scala 2.8 SBT compiler interface isn't built on Java 8.
 
  * Download and install Java 7 (setup your JAVA_HOME if needed)
  * Download and install SBT 
@@ -13,5 +14,19 @@ The programm will show further instructions. To make somethings fancier you coul
 sbt 'run VPAIDSoundTest.swf dump' # print a readable binary dump of SWF/SWC file
 sbt 'run VPAIDSoundTest.swf proxify' # adds a hook see Hook.as for each addEventListener call right in binary file
 sbt 'run VPAIDSoundTest.swf mainClass' # prints a main class of the SWF file
+sbt 'run VPAIDSoundTest.swf soundCheck' # detects if SWF has access to SoundMixer.soundTransfrom = and prints those places
 ```
+
+# Compiling
+
+```
+sbt assembly
+```
+
+JAR file will be produced after that which could be run as:
+
+```
+java -jar target/scala-2.8.1/as3-proxy-assembly-0.1-SNAPSHOT.jar VPAIDSoundTest.swf soundCheck
+```
+
 
--- src/main/scala/Tool.scala
+++ src/main/scala/Tool.scala
@@ -2,9 +2,9 @@ import apparat.swf._
 import apparat.abc._
 import apparat.bytecode._
 import apparat.utils._
-import combinator._
+import apparat.bytecode.combinator._
 import BytecodeChains._
-import operations._
+import apparat.bytecode.operations._
 import apparat.abc.analysis._
 import java.io.FileOutputStream
 
@@ -27,13 +27,14 @@ object SwfTool {
 
   def printUsage() {
     val usage = """
-      |  Usage: ${className} file.swf {proxify, mainClass, dump}
+      |  Usage: ${className} file.swf {proxify, mainClass, dump, soundCheck}
       |
       |  Where:
       |    file.swf - path to file you want work with
       |
       |    proxify - make an addEventListener proxied version of the swf
       |    mainClass - print a mainclass name
+      |    soundCheck - find SoundMixer usage
       |    dump - prints bytecode dump on the screen""" .stripMargin
     println(new Formatter(usage).format {
         "className" -> SwfTool.getClass.getName :: Nil
@@ -50,6 +51,8 @@ object SwfTool {
           proxify(file)
         case Some("mainClass") =>
           mainClass(file)
+        case Some("soundCheck") =>
+          findSoundIssues(file)
         case Some("dump") =>
           dump(file)
         case _ =>
@@ -135,13 +138,90 @@ object SwfTool {
       }
   }
 
+  def findSoundIssues(file: String) {
+    SwfTags.tagFactory = (kind: Int) => kind match {
+      case SwfTags.DoABC => Some(new DoABC)
+      case SwfTags.DoABC1 => Some(new DoABC)
+      case _ => None
+    }
+
+    val container = TagContainer fromFile file
+    val violations = container.tags.collect(findSoundMixerUsage).reduceLeft(_ ++ _)
+
+    if (!violations.isEmpty) {
+      println("Global SoundMixer access detected!")
+      violations.foreach {
+        case (method, ops) =>
+          println("Method: " + method.name)
+          ops.foreach(println)
+      }
+      System.exit(1)
+    }
+    else {
+      println("No global sound access detected")
+      System.exit(0)
+    }
+  }
+
+  def findGlobalMixerReset(method:AbcMethod):List[AbstractOp] = {
+    val result = for {
+      body <- method.body
+      bytecode <- body.bytecode
+      ops = bytecode.ops
+    } yield {
+      val soundMixerUsageStart = ops.dropWhile {
+        case FindPropStrict(AbcQName('SoundMixer, AbcNamespace(AbcNamespaceKind.Package, Symbol("flash.media")))) => false
+        case _ => true
+      }
+
+      def streamIt[T, B](coll: List[T], v: B)(f: (T, B) => B): Stream[(T, B)] =
+        coll match {
+          case e :: _ =>
+            val nv = f(e, v)
+            (e, nv) #:: streamIt(coll.tail, nv)(f)
+          case _ => Stream.empty
+        }
+
+      val opsWithStackSizes = streamIt(soundMixerUsageStart, 0) {
+        case (op, stackSize) =>
+          stackSize + op.pushOperands - op.popOperands
+      }
+
+      val soundMixerOps = {
+        val (ops, nextOps) = opsWithStackSizes.span(_._2 != 0)
+        ops #::: nextOps.take(1)
+      }.map(_._1)
+
+      val soundMixerTouched = soundMixerOps.exists {
+        case SetProperty(AbcQName('soundTransform, AbcNamespace(AbcNamespaceKind.Package, Symbol("")))) => true
+        case _ => false
+      }
+
+      if (soundMixerTouched) soundMixerOps.toList else Nil
+    }
+    result.getOrElse(Nil)
+  }
+
+
+  private def findSoundMixerUsage: PartialFunction[SwfTag, List[(AbcMethod, List[AbstractOp])]] = {
+    case doABC: DoABC =>
+      val abc = Abc fromDoABC doABC
+      abc.loadBytecode()
+      for {
+        method <- abc.methods.toList
+        ops = findGlobalMixerReset(method)
+        if !ops.isEmpty
+      } yield (method, ops)
+
+  }
+
   /**
    * This method can be used to block stage access from 3rd part libraries (such as GoogleMap)
    * by implementing a handler which will maintain a real addEventListener logic
    */
   private def replaceAddEventListener(bytecode: Bytecode) = {
     val ops = bytecode.ops
-    
+
     // try to see if bytecode has addEventListener call operation
     ops.find {
       case CallProperty(AbcQName('addEventListener, _), _) => true
@@ -166,7 +246,7 @@ object SwfTool {
       val newChain = FindPropStrict(proxyClass) ::
         GetProperty(proxyClass) ::
         paramOps :::
-        CallProperty(proxyMethod, addEventListenerOp.popOperands) :: 
+        CallProperty(proxyMethod, addEventListenerOp.popOperands) ::
         (if (addEventListenerOp.opCode == Op.callpropvoid) Pop() :: Nil else Nil)
 
       // replace the chain with proxy call chain
